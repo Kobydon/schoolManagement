4cde428d53fec41c2e8e430cbdb1bed2d874840a
Kobydon <94402053+Kobydon@users.noreply.github.com>
2024-04-18 07:56:05 +0000

    ldkl

--- application/student_view/student.py
+++ application/student_view/student.py
@@ -1117,8 +1117,8 @@ def update_general_remark():
 def get_broadsheet():
     user = User.query.filter_by(id= flask_praetorian.current_user().id).first()
     stf = Staff.query.filter_by(staff_number=user.username).first()
-    clss = Class.query.filter_by(staff_number = stf.staff_number).first()
-    rmk = BroadSheet.query.filter_by( BroadSheet.class_name==clss.class_name, BroadSheet.school_name==user.school_name)
+    cls = Class.query.filter_by(staff_number = stf.staff_number).first()
+    rmk = BroadSheet.query.filter_by( class_name=cls.class_name,school_name=stf.school_name)
     result = student_schema.dump(rmk)
     return jsonify(result)
 
