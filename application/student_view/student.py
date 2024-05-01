cf8435543db12be067fdf6d22393eee9d2d1c729
Kobydon <94402053+Kobydon@users.noreply.github.com>
2024-05-01 23:18:32 +0000

    k

--- application/student_view/student.py
+++ application/student_view/student.py
@@ -602,7 +602,7 @@ def add_result_by_excel():
           remark  = "GOOD"
           # stf = User.query.filter_by(id = flask_praetorian.current_user().id).first()
           st = Student.query.filter_by(student_number = request.json["student_number"]).first()
-          bd = BroadSheet.query.filter_by(student_number=student_number).first()
+          bd = BroadSheet.query.filter_by(student_number=st.student_number).first()
           # midterm_score  = request.json["midterm_score"]
           class_name = bd.class_name
          
