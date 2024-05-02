2f8e6b04b2420be34e670e294640348486f6b066
Kobydon <94402053+Kobydon@users.noreply.github.com>
2024-05-02 17:07:14 +0000

    c_id

--- application/student_view/student.py
+++ application/student_view/student.py
@@ -990,21 +990,21 @@ def get_grade_analysis():
     term = acd.term
     today = datetime.today()
     year=  today.year
-    # if(stf.form_master=="yes"):
-    cls= Class.query.filter_by(staff_number=stf.staff_number,school_name=user.school_name).first()
-    if (cls.class_name =="JHS 1A" or cls.class_name=="JHS 1B"):
+    if(stf.form_master=="yes"):
+        cls= Class.query.filter_by(staff_number=stf.staff_number,school_name=user.school_name).first()
+        if (cls.class_name =="JHS 1A" or cls.class_name=="JHS 1B"):
                     c_name = cls.class_name[:5] 
                     
-    elif (cls.class_name =="JHS 2A" or cls.class_name=="JHS 2B"):
+        elif (cls.class_name =="JHS 2A" or cls.class_name=="JHS 2B"):
                     c_name = cls.class_name[:5] 
-    else:
+        else:
                 c_name =cls.class_name
          
-    grd = Grading.query.filter_by(class_name=c_name,term=term,year=str(year),school_name=user.school_name).all()
+        grd = Grading.query.filter_by(class_name=c_name,term=term,year=str(year),school_name=user.school_name).all()
     
-    # if(stf.form_master!="no"):
-    #     # cls= Class.query.filter_by(class_name=stf.class_name).first()
-    #     grd = Grading.query.filter_by(subject_name=stf.subject_name,term=term,year=str(year),school_name=user.school_name).all()
+    if(stf.form_master=="no"):
+        # cls= Class.query.filter_by(class_name=stf.class_name).first()
+        grd = Grading.query.filter_by(created_by_id=flask_praetorian.current_user().id,term=term,year=str(year),school_name=user.school_name).all()
        
     result = student_schema.dump(grd)
     return jsonify(result)   
