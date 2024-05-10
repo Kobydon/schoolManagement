d17bbf744ec9ca33b56d34c3c6986780ecb31b12
Kobydon <94402053+Kobydon@users.noreply.github.com>
2024-05-10 23:53:39 +0000

    :th

--- application/student_view/student.py
+++ application/student_view/student.py
@@ -844,14 +844,16 @@ def all_total():
     subject_name=  request.json["subject_name"]
     # t = Student.query.filter_by(student_number=student_number).first()
     bd  = BroadSheet.query.filter_by(student_number=student_number).first()
-
+    
+    if(canpost=="skip"):
+       return jsonify("skip")
    
-  
-    tt=int(bd.all_total)
-    bd.all_total=tt+tot
+    else:
+         tt=int(bd.all_total)
+         bd.all_total=tt+tot
         # t.all_total = all_total
          c="n"
-    db.session.commit()
+         db.session.commit()
     # grd = Student.query.filter(Student.class_name==t.class_name )
 # brd =  BroadSheet.query.filter(BroadSheet.class_name==bd.class_name)
     acd = Academic.query.filter_by(school_name=user.school_name,status="current").first()
