52af545df91d9fbc20aebf1079bad28caaad4fff
Kobydon <94402053+Kobydon@users.noreply.github.com>
2024-05-14 16:14:46 +0000

    ddw

--- application/student_view/student.py
+++ application/student_view/student.py
@@ -826,6 +826,9 @@ def all_total():
         canpost = request.json["canpost"]
         # tot =int(all_total)
         user = User.query.filter_by(id = flask_praetorian.current_user().id).first()
+    
+
+  
   
  
         student_number = request.json["student_number"]
@@ -842,20 +845,25 @@ def all_total():
         year=  today.year
         bd = BroadSheet.query.filter_by(student_number=student_number).first()
         c =bd.class_name
-        broadsheets =  BroadSheet.query.filter_by(class_name=c,school_name=user.school_name, term =term,year=acd.year)
-        lsti= brd.order_by(desc(BroadSheet.all_total)).all()
+        brd =  BroadSheet.query.filter_by(class_name=c,school_name=user.school_name, term =term,year=acd.year)
+        lst1= brd.order_by(desc(BroadSheet.all_total)).all()
+
+        for(pos,g) in enumerate(lst1):
+            
+                    g.pos = pos+1
+                
+            
+
+            
         
-        for(pos,g) in enumerate(lsti):
-          
-            g.pos = pos+1
-            
-         
-            
+
         db.session.commit()
         db.session.close()
+        
         resp = jsonify("Success")
         resp.status_code=200
         return  resp    
+            
  
  
  
