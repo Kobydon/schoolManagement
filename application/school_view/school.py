d7c745b5e1bd7e94576b48429136c07599833c56
Kobydon <94402053+Kobydon@users.noreply.github.com>
2024-05-30 12:34:21 +0000

    wefg

--- application/school_view/school.py
+++ application/school_view/school.py
@@ -35,7 +35,7 @@ class classSchema(ma.Schema):
         
 class schemeSchema(ma.Schema):
     class Meta:
-        fields=("id","exams_score","subject_name","midterm_score","class_score"
+        fields=("id","exams_score","subject_name","midterm_score","class_score","default"
 )
    
    
