08edbca272f0b26a936c8edd5abc979c9f613411
Kobydon <94402053+Kobydon@users.noreply.github.com>
2024-09-08 14:12:31 +0000

    seddc

--- application/school_view/school.py
+++ application/school_view/school.py
@@ -2778,7 +2778,7 @@ def add_allowance():
     user = User.query.filter_by(id = flask_praetorian.current_user().id).first()
     name= request.json["name"]
     amount =request.json["amount"]
-    role= request["role"]
+    role= request.json["role"]
     # date =request.json["date"]
     # usr = user.firstname +" " + user.lastname
     created_date=datetime.now().strftime('%Y-%m-%d %H:%M')
