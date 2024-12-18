6ac638e4e44190432660c4d2caa71708108f8082
Kobydon <94402053+Kobydon@users.noreply.github.com>
2024-12-18 06:19:49 +0000

    changer

--- application/student_view/student.py
+++ application/student_view/student.py
@@ -95,7 +95,7 @@ def add_student():
         admitted_year=admitted_year,
         residential_status=residential_status,
         class_name=class_name,
-        student_number=student_number,
+        student_number=request.json["student_number"],
         picture=picture,
         school_name=school_name,
         created_date=datetime.now().strftime('%Y-%m-%d %H:%M'),
@@ -127,9 +127,9 @@ def add_student():
     )
 
     # Add and commit to the database
-    db.session.add(std)
+    # db.session.add(std)
     db.session.add(bd)
-    db.session.add(user_entry)
+    # db.session.add(user_entry)
     db.session.commit()
 
     return jsonify("success"), 200
