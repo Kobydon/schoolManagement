9c8bb0863854bc12036176023514a25daac90786
Kobydon <94402053+Kobydon@users.noreply.github.com>
2024-12-18 07:24:34 +0000

    ll

--- application/student_view/student.py
+++ application/student_view/student.py
@@ -95,7 +95,7 @@ def add_student():
         admitted_year=admitted_year,
         residential_status=residential_status,
         class_name=class_name,
-        student_number=student_number,
+        student_number=request.json["student_number"],
         picture=picture,
         school_name=school_name,
         created_date=datetime.now().strftime('%Y-%m-%d %H:%M'),
@@ -105,7 +105,7 @@ def add_student():
     bd = BroadSheet(
         student_name=student_name,
         class_name=c_name,
-        student_number=request.json["student_number"],
+        student_number=student_number,
         school_name=school_name,
         original_class_name=class_name,
         current_status="",
@@ -127,8 +127,8 @@ def add_student():
     )
 
     # Add and commit to the database
-    # db.session.add(std)
-    db.session.add(bd)
+    db.session.add(std)
+    # db.session.add(bd)
     # db.session.add(user_entry)
     db.session.commit()
 
